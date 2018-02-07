6f1f4577245f0078aca993788839a6b00215fc2f
Eric Fiorani <efiorani305@gmail.com>
2018-02-07 13:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ function takeANumber(katzDeliLine, name){
   return "Welcome, " + name + ". You are number " + orderInLine + " in line.";
 }
 
-function nowServing(katzDeli){
-  let i = 0;
-  while (i < katzDeli.length){
-    i++;
-  }
-  if (katzDeli.length === 0){
-    return "There is nobody waiting to be served!";
- } else {
-    return ('Currently serving ${katzDeli.shift()}.');
-  }
+function nowServing(firstInLine){
+  
 }
