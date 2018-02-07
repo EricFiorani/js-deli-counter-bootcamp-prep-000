fe4f091b96560f59b09bec39d38de4f15e3b3e78
Eric Fiorani <efiorani305@gmail.com>
2018-02-07 13:11:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push('${name}');
-  return('Welcome, ${name}. You are Number ' + katzDeli.length + ' in line.');
+  var orderInLine = katzDeliLine.length + 1
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number" + orderInLine + " in line."
 }
 
 function nowServing(katzDeli){
