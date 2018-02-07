3c90f955ada29f56ecfbed0fa918aa464f3d86ae
Eric Fiorani <efiorani305@gmail.com>
2018-02-07 12:53:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
  } else {
-    return ('Currently serving katzDeliLine.shift().');
+    var currentlyUp = KatzDeliLine.shift()
+    return ('Currently serving' + currentlyUp + ".");
   }
 }
