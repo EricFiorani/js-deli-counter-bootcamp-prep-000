8255eca5f5b1e921e91d3ea72af9b406c52ed6b8
Eric Fiorani <efiorani305@gmail.com>
2018-02-07 13:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length == 0){
     return "The line is currently empty.";
   } else {
-    var lineList = "The line is currently: 1. ${katzDeliLine[0]}";
+    var lineList = "The line is currently: 1. " + katzDeliLine[0] + ".";
     for (let i = 1; i < katzDeliLine.length; i++){
       lineList += ", ${i + 1}. ${katzDeliLine[i]}"
     }
